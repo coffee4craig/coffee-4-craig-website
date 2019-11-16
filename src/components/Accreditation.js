5eeba7abfd66cf480d30b43882c81277c292ec44
Catherine McDonough <catherine.mcdonough@deptagency.com>
2019-11-16 11:48:57 +0000

    tidy up of files

---
+++ src/components/Accreditation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Accreditation = () => (
+  <div className="accreditation">
+    <h4>Accreditation</h4>
+  </div>
+)
+
+export default Accreditation;
---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Card = () => (
+  <div className="card">
+    <h4>Card</h4>
+    <button type="button" className="btn">
+      Button
+    </button>
+  </div>
+)
+
+export default Card;
