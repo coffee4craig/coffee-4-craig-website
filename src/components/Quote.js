4cc54142dc436f7d39e9700dcba3dcbef8f785bc
Catherine McDonough <catherine.mcdonough@deptagency.com>
2019-11-17 16:05:26 +0000

    Added quote component and styles

---
+++ src/components/Quote.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Quote = () => (
+  <div className="quote">
+    <div className="quote__wrapper">
+    <h2 className="quote__text">‘Homelessness is not the problem, it is a symptom of the problem’</h2>
+    <p className="quote__source">Hendrix Lancaster CEO</p>
+    </div>
+  </div>
+);
+
+export default Quote;
