322e5ea7914a80362d3989b71dd9a6595fc68de7
Joe Stephens <joestephens@deptagency.com>
2020-03-09 20:14:57 +0000

    Fix hero text + optional credits link

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -4,30 +4,36 @@ import PreviewCompatibleImage from './PreviewCompatibleImage';
 
 const Credits = ({ title, creditList }) => {
     return (
-    <div className="featured">
-        <div className="featured__wrapper">
-            <div className="featured__row_1">
-                <p className="featured__row_1_text">{title}</p>
-            </div>
-            <div className="featured__row_2">
-                {
-                    creditList && creditList.length > 0 && creditList.map((credit, i) => (
-                        <div key={i} className="featured___column">
-                            <a href={credit.link.url} target={credit.link.target}>
-                                <PreviewCompatibleImage cName="featured__logo" isContained imageInfo={credit.logo} />
-                            </a>
-                            {
-                                credit.description && credit.description.length > 0 &&
+        <div className="featured">
+            <div className="featured__wrapper">
+                <div className="featured__row_1">
+                    <p className="featured__row_1_text">{title}</p>
+                </div>
+                <div className="featured__row_2">
+                    {
+                        creditList && creditList.length > 0 && creditList.map((credit, i) => (
+                            <div key={i} className="featured___column">
+                                {
+                                    credit.link.url !== '#' ? (
+                                        <a href={credit.link.url} target={credit.link.target}>
+                                            <PreviewCompatibleImage cName="featured__logo" isContained imageInfo={credit.logo} />
+                                        </a>
+                                    ) : (
+                                            <PreviewCompatibleImage cName="featured__logo" isContained imageInfo={credit.logo} />
+                                        )
+                                }
+                                {
+                                    credit.description && credit.description.length > 0 &&
                                     <p className="featured__description">
                                         {credit.description}
                                     </p>
-                            }
-                        </div>
-                    ))
-                }
+                                }
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
-        </div>   
-    </div>
+        </div>
     )
 };
 
