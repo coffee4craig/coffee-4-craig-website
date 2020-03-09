f9a8d693ab156b0330079027860521105267e77c
LukeBrannagan <luke.brannagan@deptagency.com>
2020-03-09 19:01:13 +0000

    fixed the issue

--- src/components/LeadText.js
+++ src/components/LeadText.js
@@ -4,7 +4,10 @@ const LeadText = ({ text }) => (
   <div className="lead-text">
     <div className="lead-text__wrapper">
       <div class="lead-text__col">
-        <div class="lead-text__text" dangerouslySetInnerHTML={{ text }}></div>
+        <div
+          class="lead-text__text"
+          dangerouslySetInnerHTML={{ __html: text }}
+        ></div>
       </div>
     </div>
   </div>
