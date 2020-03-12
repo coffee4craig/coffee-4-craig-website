dbc548ccf2168cc7b0e946e2d3800662b520f71c
Matei Potcovaru <matei.potcovaru@deptagency.com>
2020-03-12 11:42:07 +0000

    seo image and content

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Coffee 4 Craig',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Coffee4Craig provides vital support for Manchester’s homeless and people in crisis',
+    image: "/img/facebook__seo.jpg"
+
   },
   plugins: [
     'gatsby-plugin-react-helmet',
